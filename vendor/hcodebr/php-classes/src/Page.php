31a71c84c9d36dc4554620f887ddb53f97e5324e
Thiago Costa <thiago_8777@hotmail.com>
2018-06-20 00:30:45 -0300

    Criando classe PageAdmin e View Admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,13 +10,13 @@ class Page
 	private $options = array();
 	private $defaults = array("data" => array());
 
-	public function __construct($opts = array())
+	public function __construct($opts = array(), $tpl_dir = "/views/")
 	{
 
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . "/views/",
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . $tpl_dir,
 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/cache/",
 			"debug"         => false 
 		);
